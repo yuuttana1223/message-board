6b542932c57b0f52642b133d3361781996e121ae
yuuttana1223 <yutanadreamfilel.seed@gmail.com>
2020-12-29 20:53:54 +0900

    DRY

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -1,10 +1,12 @@
 class MessagesController < ApplicationController
+  # @message = Message.find(params[:id])を共通化
+  before_action :set_message, only: [:show, :edit, :update, :destroy]
+
   def index
     @messages = Message.all
   end
 
   def show
-    @message = Message.find(params[:id])
   end
 
   def new
@@ -17,39 +19,43 @@ class MessagesController < ApplicationController
       # { success: 'Message が正常に投稿されました' } という形で保存
       flash[:success] = 'Message が正常に投稿されました'
       # HTTP リクエストを発生
-      # message_url(@message.id)でも可 (urlは絶対パス)
+      # @messageだけでも可 (urlは絶対パス)
       redirect_to message_url(@message.id)
     else 
       flash.now[:danger] = 'Message が投稿されませんでした'
+      # HTTP リクエストを発生させない
+      # 'new', :newでも可
       render new_message_path
     end
   end
 
   def edit
-    @message = Message.find(params[:id])
   end
 
   def update
-    @message = Message.find(params[:id])
-
     if @message.update(message_params)
       flash[:success] = 'Message は正常に更新されました'
+      # @messageだけでも可
       redirect_to message_url(@message.id)
     else 
       flash.now[:danger] = 'Message は更新されませんでした'
+      # 'edit', :editでも可
       redirect_to edit_message_url(@message.id)
     end
   end
 
   def destroy
-    @message = Message.find(params[:id])
     @message.destroy
-
     flash[:success] = 'Message は正常に削除されました'
     redirect_to messages_url
   end
-
+  
   private
+
+    def set_message
+      @message = Message.find(params[:id])
+    end
+    # Strong Parameter
     def message_params
       params.require(:message).permit(:content)
     end

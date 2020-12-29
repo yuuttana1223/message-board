155fe0c3368a7e8b59b1395a863a5effdf61ae07
yuuttana1223 <yutanadreamfilel.seed@gmail.com>
2020-12-29 23:56:54 +0900

    add pagination

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -3,7 +3,7 @@ class MessagesController < ApplicationController
   before_action :set_message, only: [:show, :edit, :update, :destroy]
 
   def index
-    @messages = Message.all
+    @messages = Message.all.page(params[:page]).per(3)
   end
 
   def show
